913ccc58c5a12ec7e93248222e183c6764cf7220
Sarah Knuth <smk5926@psu.edu>
2019-02-18 19:12:42 -0500

    Added Insurance History Form

--- InsuranceWizardsApp/app/src/main/java/com/capstone/insuranceapp/DriverHouseholdForm.java
+++ InsuranceWizardsApp/app/src/main/java/com/capstone/insuranceapp/DriverHouseholdForm.java
@@ -222,7 +222,7 @@ public class DriverHouseholdForm extends AppCompatActivity {
                 if(validateInput()){
                     // this will either send data to firebase or to next section of the form
 
-                    Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                    Intent intent = new Intent(getApplicationContext(), InsuranceHistoryForm.class);
                     startActivity(intent);
                 }
             }
