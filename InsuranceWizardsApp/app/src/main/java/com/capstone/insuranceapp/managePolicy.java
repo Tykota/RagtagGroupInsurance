bd90d50d6026bb0477f2a2ccbc65ab1ee9cfd91a
Sarah Knuth <smk5926@psu.edu>
2019-03-12 22:21:32 -0400

    Accessing firestore database

--- InsuranceWizardsApp/app/src/main/java/com/capstone/insuranceapp/managePolicy.java
+++ InsuranceWizardsApp/app/src/main/java/com/capstone/insuranceapp/managePolicy.java
@@ -2,6 +2,7 @@ package com.capstone.insuranceapp;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -11,11 +12,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class managePolicy extends AppCompatActivity {
     @Override
@@ -23,31 +29,20 @@ public class managePolicy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_manage_policy);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance("clients");
-        DatabaseReference myRef = database.getReference("clients");
-
-
-        //Populate list of cars from Database
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-
-                Log.d("SWAG", "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("SWAG", "Failed to read value.", error.toException());
-            }
-        });
-
-
-
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection("clients").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("SUCCESS", document.getId() + " = " + document.getData());
+                            }
+                        } else {
+                            Log.w("Error", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 
 }
