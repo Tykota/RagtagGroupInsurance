fd259624534842bc5e55e6799e949dbed7f047e7
Sarah Knuth <smk5926@psu.edu>
2019-04-09 20:01:38 -0400

    Added MonitorDrive activity

---
+++ InsuranceWizardsApp/app/src/main/java/com/capstone/insuranceapp/MonitorDrive.java
@@ -0,0 +1,13 @@
+package com.capstone.insuranceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonitorDrive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor_drive);
+    }
+}
