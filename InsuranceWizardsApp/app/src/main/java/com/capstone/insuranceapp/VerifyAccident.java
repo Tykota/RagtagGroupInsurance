10d6d0059e966f3d962999bf55ee78cdfe22c265
Sarah Knuth <smk5926@psu.edu>
2019-04-15 19:28:05 -0400

    Update VerifyAccident.java

--- InsuranceWizardsApp/app/src/main/java/com/capstone/insuranceapp/VerifyAccident.java
+++ InsuranceWizardsApp/app/src/main/java/com/capstone/insuranceapp/VerifyAccident.java
@@ -2,12 +2,33 @@ package com.capstone.insuranceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VerifyAccident extends AppCompatActivity {
+
+    private Button helpBtn, fineBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_accident);
+
+        helpBtn = findViewById(R.id.help_btn);
+        helpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // call for help
+            }
+        });
+
+        fineBtn = findViewById(R.id.fine_btn);
+        fineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // close activity
+                finish();
+            }
+        });
     }
 }
